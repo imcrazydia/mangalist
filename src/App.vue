5cc53d7e30511c764edcbd092f1b03739bb89531
diaquino <25682@ma-web.nl>
2020-01-31 18:50:18 +0100

    data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      mangas: [
+        {
+          id: 1,
+          title: "Shield Hero",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Iron ladies",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Solo leveling",
+          completed: true
+        }
+      ]
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
